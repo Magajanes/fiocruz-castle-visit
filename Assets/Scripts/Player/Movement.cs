87047534b9f1f5c62a2cd0eaa5c6a3d18a9cedb7
magajanes <thiagosmfis@gmail.com>
2020-12-05 17:18:38 -0300

    Add gravity and start implementing colliders

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -2,13 +2,16 @@
 
 public class Movement : MonoBehaviour
 {
-    private Vector3 _movementX;
-    private Vector3 _movementY;
+    private Vector3 _walkDirection;
+    private Vector3 _strafeDirection;
+    private Vector3 _verticalDirection;
     private Vector3 _moveDirection;
 
     [Header("Parameters")]
     [SerializeField]
     private float speed;
+    [SerializeField]
+    private float gravityModifier;
 
     [Header("References")]
     [SerializeField]
@@ -28,12 +31,24 @@ public class Movement : MonoBehaviour
         InputController.OnTurnInput -= TurnHeadTowards;
     }
 
-    private void MoveTowards(Vector2 inputDirection)    
+    private void MoveTowards(Vector2 inputDirection)
     {
-        _movementX = inputDirection.x * transform.right;
-        _movementY = inputDirection.y * transform.forward;
-        _moveDirection = _movementX + _movementY;
+        _walkDirection = inputDirection.x * transform.right;
+        _strafeDirection = inputDirection.y * transform.forward;
+        CalculateGravity();
+        _moveDirection = _walkDirection + _strafeDirection + _verticalDirection;
         characterController.Move(_moveDirection * speed * Time.deltaTime);
+    }
+
+    private void CalculateGravity()
+    {
+        _verticalDirection = _moveDirection.y * Vector3.up;
+        if (characterController.isGrounded)
+        {
+            _verticalDirection.y = Physics.gravity.y * gravityModifier * Time.deltaTime;
+            return;
+        }
+        _verticalDirection.y += Physics.gravity.y * gravityModifier * Time.deltaTime;
     }
 
     private void TurnHeadTowards(Vector2 mouseInput)
