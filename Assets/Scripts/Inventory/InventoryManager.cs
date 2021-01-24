40d15bd7bf827ab7ff329f20c516d3359f0805ff
Magajanes <thiagosmfis@gmail.com>
2021-01-23 23:01:45 -0300

    Remove InventoryService

--- Assets/Scripts/Inventory/InventoryManager.cs
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -1,19 +1,32 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class InventoryManager : MonoBehaviour
 {
+    public static Inventory PlayerInventory;
+    
     public void Initialize()
     {
         InputController.OnInventoryButtonPress += ShowInventory;
         InputController.OnBackButtonPress += CloseInventory;
 
-        InventoryService.InitializeInventory();
+        InitializeInventory();
     }
 
-    private void OnDestroy()
+    public void InitializeInventory()
     {
-        InputController.OnInteractionButtonPress -= ShowInventory;
-        InputController.OnBackButtonPress -= CloseInventory;
+        PlayerInventory = new Inventory();
+        PlayerInventory.Initialize();
+    }
+
+    public void SaveArtifact(int artifactId)
+    {
+        PlayerInventory.AddArtifact(artifactId);
+    }
+
+    public List<int> GetCollectedArtifactsIds()
+    {
+        return PlayerInventory.GetCollectedArtifactsIds();
     }
 
     private void ShowInventory()
@@ -24,5 +37,11 @@ public class InventoryManager : MonoBehaviour
     private void CloseInventory()
     {
         UIManager.ChangeState(UIState.Inactive);
+    }
+
+    private void OnDestroy()
+    {
+        InputController.OnInteractionButtonPress -= ShowInventory;
+        InputController.OnBackButtonPress -= CloseInventory;
     }
 }
