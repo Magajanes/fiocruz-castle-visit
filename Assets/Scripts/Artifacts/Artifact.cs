b12a3849f3e96ffa8558479514d6907f3e026e1d
Thiago Magalhães <thiagosmfis@gmail.com>
2021-11-14 19:56:16 -0300

    create importer to duplicate artifacts scriptable object

--- Assets/Scripts/Artifacts/Artifact.cs
+++ Assets/Scripts/Artifacts/Artifact.cs
@@ -82,5 +82,31 @@ public class ArtifactInfo
     public int Id;
     public string Name;
     public string Description;
+    public string StylizedImagePath;
+    public string ThumbnailImagePath;
     public string ImagePath;
+
+    public static ArtifactInfo Copy(ArtifactInfo artifactInfo)
+    {
+        var newArtifactInfo = new ArtifactInfo();
+        newArtifactInfo.Id = artifactInfo.Id;
+        newArtifactInfo.Name = artifactInfo.Name;
+        newArtifactInfo.Description = artifactInfo.Description;
+        newArtifactInfo.StylizedImagePath = artifactInfo.StylizedImagePath;
+        newArtifactInfo.ThumbnailImagePath = artifactInfo.ThumbnailImagePath;
+        newArtifactInfo.ImagePath = artifactInfo.ImagePath;
+        return newArtifactInfo;
+    }
+
+    public static ArtifactInfo CopyAndOverrideId(ArtifactInfo artifactInfo, int newId)
+    {
+        var newArtifactInfo = new ArtifactInfo();
+        newArtifactInfo.Id = newId;
+        newArtifactInfo.Name = artifactInfo.Name;
+        newArtifactInfo.Description = artifactInfo.Description;
+        newArtifactInfo.StylizedImagePath = artifactInfo.StylizedImagePath;
+        newArtifactInfo.ThumbnailImagePath = artifactInfo.ThumbnailImagePath;
+        newArtifactInfo.ImagePath = artifactInfo.ImagePath;
+        return newArtifactInfo;
+    }
 }
---
+++ Assets/Scripts/Utils/ArtifactsImporter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ArtifactsImporter : MonoBehaviour
+{
+    [SerializeField]
+    private GameArtifacts _origin;
+
+    [SerializeField]
+    private GameArtifacts _overrided;
+
+    [SerializeField]
+    private List<int> _itemsToRemove;
+
+    private void Start()
+    {
+        _overrided.Artifacts.Clear();
+        int idToAdd = 1;
+
+        for (int i = 0; i < _origin.Artifacts.Count; i++)
+        {
+            if (_itemsToRemove.Contains(_origin.Artifacts[i].Id)) continue;
+
+            _overrided.Artifacts.Add(ArtifactInfo.CopyAndOverrideId(_origin.Artifacts[i], idToAdd));
+            idToAdd++;
+        }
+    }
+}
