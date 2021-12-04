8f6e2acc37b1ed9ee58b3fc9e66deab45e4b8031
Thiago Magalhães <thiagosmfis@gmail.com>
2021-12-04 19:54:35 -0300

    fix light bugs

---
+++ Assets/Scripts/Utils/SpecialLightTrigger.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class SpecialLightTrigger : LightTrigger, ILightTrigger
+{
+    [SerializeField]
+    private GameObject _light;
+    
+    public void Initialize()
+    {
+        OnLightTriggerEnter += () =>
+        {
+            _light.SetActive(false);
+        };
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        _light.SetActive(true);
+    }
+}
