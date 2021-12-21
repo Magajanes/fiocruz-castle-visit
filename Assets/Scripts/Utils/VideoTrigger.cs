9c0318ba3c59379aa112b3fb80f91a3b284ac67b
Thiago Magalhães <thiagosmfis@gmail.com>
2021-12-20 23:06:40 -0300

    trigger video

---
+++ Assets/Scripts/Utils/VideoTrigger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private VideoPlayer _videoPlayer;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        InputController.OnInteractionButtonPress -= StartVideo;
+        InputController.OnInteractionButtonPress += StartVideo;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        InputController.OnInteractionButtonPress -= StartVideo;
+    }
+
+    private void StartVideo()
+    {
+        _videoPlayer.Play();
+        _videoPlayer.isLooping = true;
+        InputController.OnInteractionButtonPress -= StartVideo;
+    }
+}
