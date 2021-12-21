6ea34c1dfcbca1c552976bc0f798d19409ff1df6
Thiago Magalhães <thiagosmfis@gmail.com>
2021-12-21 19:07:54 -0300

    fix credits screen and optimize video trigger

--- Assets/Scripts/Utils/VideoTrigger.cs
+++ Assets/Scripts/Utils/VideoTrigger.cs
@@ -6,6 +6,8 @@ public class VideoTrigger : MonoBehaviour
     [SerializeField]
     private VideoPlayer _videoPlayer;
 
+    private bool _videoPlaying = false;
+
     private void OnTriggerEnter(Collider other)
     {
         InputController.OnInteractionButtonPress -= StartVideo;
@@ -13,14 +15,18 @@ public class VideoTrigger : MonoBehaviour
     }
 
     private void OnTriggerExit(Collider other)
-    {
+    {   
         InputController.OnInteractionButtonPress -= StartVideo;
     }
 
     private void StartVideo()
     {
+        InputController.OnInteractionButtonPress -= StartVideo;
+        if (_videoPlaying) return;
+
         _videoPlayer.Play();
         _videoPlayer.isLooping = true;
-        InputController.OnInteractionButtonPress -= StartVideo;
+
+        _videoPlaying = true;
     }
 }
